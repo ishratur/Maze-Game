7bd80dfcd384d14934620884235a67c150c46ff0
Ishratur Rahman <shourov100@yahoo.com>
2021-03-13 12:26:27 -0800

    Update Main.java

--- src/ca/cmpt213/asn2/Model/Main.java
+++ src/ca/cmpt213/asn2/Model/Main.java
@@ -2,12 +2,6 @@ package ca.cmpt213.asn2.Model;
 
 import ca.cmpt213.asn2.Model.Grid;
 
-/*
- * Collaborators:
- * Ishratur Rahman - 301278317
- * Shawn McGirr -  301358896
-* */
-
 public class Main {
 
     public static void main(String[] args) {
